50096b6eb7d549afcfc997acbdf7996ce77669b1
Jim Pivarski <jpivarski@gmail.com>
2022-11-11 19:30:09 -0600

    Don't assume the objects still exist.

--- test-animal.cpp
+++ test-animal.cpp
@@ -6,7 +6,7 @@
 #include <src-animal/animal.h>
 
 int main() {
-  std::cout << "hello" << std::endl;
+  std::cout << "BEGIN" << std::endl;
 
   std::ifstream infile("data/animal.raw", std::ifstream::binary);
 
@@ -14,13 +14,15 @@ int main() {
 
   animal_t zoo = animal_t(&ks);
 
-  std::vector<animal_t::animal_entry_t*> *animal_vec = zoo.entry();
+  // std::vector<animal_t::animal_entry_t*> *animal_vec = zoo.entry();
 
-  for (int i = 0;  i < animal_vec->size();  i++) {
-    animal_t::animal_entry_t *testAnimal = animal_vec->at(i);
+  // for (int i = 0;  i < animal_vec->size();  i++) {
+  //   animal_t::animal_entry_t *testAnimal = animal_vec->at(i);
 
-    std::cout << "Species: " << testAnimal->species() << std::endl;
-    std::cout << "Age: " << static_cast<int>(testAnimal->age()) << std::endl;
-    std::cout << "Weight: " << testAnimal->weight() << std::endl;
-  }
+  //   std::cout << "Species: " << testAnimal->species() << std::endl;
+  //   std::cout << "Age: " << static_cast<int>(testAnimal->age()) << std::endl;
+  //   std::cout << "Weight: " << testAnimal->weight() << std::endl;
+  // }
+
+  std::cout << "END" << std::endl;
 }
